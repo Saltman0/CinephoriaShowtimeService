94a8a174da583a8e93a3a7a2d8bf6b8f766a974b
Saltman0 <baudoin.mathieu@gmail.com>
2024-11-12 22:37:20 +0100

    Showtime controller added

---
+++ src/controllers/showtime.controller.ts
@@ -0,0 +1,91 @@
+import { Request, Response } from 'express';
+import * as showtimeRepository from "../repository/showtime.repository";
+
+export async function getShowtimes(req: Request, res: Response) {
+    try {
+        const showtimes = await showtimeRepository.findShowtimes(
+            parseInt(req.params.movieId),
+            req.params.startDate ? new Date(req.params.startDate) : null,
+            req.params.endDate ? new Date(req.params.endDate) : null
+        );
+
+        if (showtimes !== null) {
+            res.status(200).json(showtimes);
+        } else {
+            res.status(404).json({error : `Showtimes not found.`});
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function getShowtimeById(req: Request, res: Response) {
+    try {
+        const showtimeId: number = parseInt(req.params.id);
+
+        const showtime = await showtimeRepository.findShowtimeById(showtimeId);
+
+        if (showtime !== null) {
+            res.status(200).json(showtime);
+        } else {
+            res.status(404).json({error : `Showtime ${showtimeId} not found.`});
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function createShowtime(req: Request, res: Response) {
+    try {
+        const showtimeToCreate = await showtimeRepository.insertShowtime(
+            new Date(req.body.startTime),
+            new Date(req.body.endTime),
+            parseFloat(req.body.price),
+            parseInt(req.body.movieId),
+            parseInt(req.body.hallId)
+        );
+
+        res.status(201).json(showtimeToCreate);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function updateShowtime(req: Request, res: Response) {
+    try {
+        const showtimeToUpdate = await showtimeRepository.updateShowtime(
+            parseInt(req.params.id),
+            new Date(req.body.startTime),
+            new Date(req.body.endTime),
+            parseFloat(req.body.price),
+            parseInt(req.body.movieId),
+            parseInt(req.body.hallId)
+        );
+
+        res.status(200).json(showtimeToUpdate);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+export async function deleteShowtime(req: Request, res: Response) {
+    try {
+        const showtimeToDelete = await showtimeRepository.deleteShowtime(
+            parseInt(req.params.id)
+        );
+
+        res.status(200).json(showtimeToDelete);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
