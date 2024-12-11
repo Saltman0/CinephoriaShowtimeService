9fc2570aa354ebda42fe34e3c082c9111895476f
Saltman0 <baudoin.mathieu@protonmail.com>
2024-12-11 16:12:00 +0100

    Update createDatabaseAndTables.sql

--- src/sql/createDatabaseAndTables.sql
+++ src/sql/createDatabaseAndTables.sql
@@ -1,4 +1,4 @@
--- Cinéphoria movie service database
+-- Cinéphoria showtime service database
 DROP DATABASE IF EXISTS "cinephoriaShowtimeServiceDatabase";
 CREATE DATABASE "cinephoriaShowtimeServiceDatabase";
 -- Cinéphoria movie service database
