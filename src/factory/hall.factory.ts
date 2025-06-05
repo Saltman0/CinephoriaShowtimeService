951e8a788a57b6ae1aefa5af186bf0b567a49f98
Saltman0 <baudoin.mathieu@protonmail.com>
2025-06-05 13:57:46 +0200

    :fire: Cinema/Hall/Movie removed

--- src/factory/hall.factory.ts
+++
@@ -1,8 +0,0 @@
-export function createHall(id: number, number: number, projectionQuality: string, cinemaId: number) {
-    return {
-        id: id,
-        number: number,
-        projectionQuality: projectionQuality,
-        cinemaId: cinemaId
-    };
-}
