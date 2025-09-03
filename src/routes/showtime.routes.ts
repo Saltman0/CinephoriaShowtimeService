d1b8d661a653d961e4303402ce252b31040ca382
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-03 23:13:08 +0200

    🎨 Showtime routes updated

--- src/routes/showtime.routes.ts
+++ src/routes/showtime.routes.ts
@@ -4,9 +4,9 @@ import passport from "../middleware/passport";
 
 const router: Router = Router();
 
-router.get("/showtime", passport.authenticate("jwt", { session: false }), showtimeController.getShowtimes);
-router.get("/showtime/current-showtime", passport.authenticate("jwt", { session: false }), showtimeController.getCurrentShowtimes);
-router.get("/showtime/:showtimeId", passport.authenticate("jwt", { session: false }), showtimeController.getShowtimeById);
+router.get("/showtime", showtimeController.getShowtimes);
+router.get("/showtime/current-showtime", showtimeController.getCurrentShowtimes);
+router.get("/showtime/:showtimeId", showtimeController.getShowtimeById);
 router.post("/showtime", passport.authenticate("jwt", { session: false }), showtimeController.createShowtime);
 router.put("/showtime/:showtimeId", passport.authenticate("jwt", { session: false }), showtimeController.updateShowtime);
 router.delete("/showtime/:showtimeId", passport.authenticate("jwt", { session: false }), showtimeController.deleteShowtime);
