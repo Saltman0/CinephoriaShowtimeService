ff708410fa7b970a3d2c8ad129875e3fb4695a15
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-21 23:26:12 +0200

    ✨ Migration updated

--- migrations/0001_rework.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE "cinema";--> statement-breakpoint
-DROP TABLE "hall";--> statement-breakpoint
-DROP TABLE "movie";--> statement-breakpoint
-ALTER TABLE "showtime" DROP CONSTRAINT "showtime_movieId_movie_id_fk";
---> statement-breakpoint
-ALTER TABLE "showtime" DROP CONSTRAINT "showtime_hallId_hall_id_fk";
