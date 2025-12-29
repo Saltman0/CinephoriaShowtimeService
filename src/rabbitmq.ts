796c158fca38ec33bd322f6f10867551a578fd44
Saltman0 <baudoin.mathieu@protonmail.com>
2025-12-30 00:05:11 +0100

    ✨ RabbitMQ configuration added

---
+++ src/rabbitmq.ts
@@ -0,0 +1,21 @@
+import * as amqp from "amqplib";
+
+const URL: string = "amqp://"+process.env.RABBITMQ_USER+":"+process.env.RABBITMQ_PASSWORD+"@"+process.env.RABBITMQ_IP;
+
+export async function publishMessage(exchange: string, message: string) {
+    try {
+        const connection: amqp.ChannelModel = await amqp.connect(URL);
+        const channel: amqp.Channel = await connection.createChannel();
+
+        await channel.assertExchange(exchange, "fanout", { durable: false });
+
+        channel.publish(exchange, "", Buffer.from(message));
+        console.log(`Sent: ${message}`);
+
+        setTimeout(function () {
+            connection.close();
+        }, 500);
+    } catch (error) {
+        console.error(error);
+    }
+}
